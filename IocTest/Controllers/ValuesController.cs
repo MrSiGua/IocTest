8a47cefcee0b990e0ec279910af8ca799179a34f
SiGua <1058435977@qq.com>
2019-08-06 16:22:46 +0800

    添加项目文件。

---
+++ IocTest/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IocTest.Models;
+
+namespace IocTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ISingTest sing; ITranTest tran; ISconTest scon; IAService aService;
+
+        public ValuesController(ISingTest sing, ITranTest tran, ISconTest scon, IAService aService)
+        {
+            this.sing = sing;
+            this.tran = tran;
+            this.scon = scon;
+            this.aService = aService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> SetTest()
+        {
+            sing.Age = 18;
+            sing.Name = "小红";
+
+            tran.Age = 19;
+            tran.Name = "小明";
+
+            scon.Age = 20;
+            scon.Name = "小蓝";
+
+            aService.RedisTest();
+
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IocTest/Models/Model.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IocTest.Models
+{
+    /*权重：
+    AddSingleton→AddTransient→AddScoped
+    AddSingleton的生命周期：
+    项目启动-项目关闭   相当于静态类  只会有一个  
+    AddScoped的生命周期：
+    请求开始-请求结束  在这次请求中获取的对象都是同一个 
+    AddTransient的生命周期：
+    请求获取-（GC回收-主动释放） 每一次获取的对象都不是同一个*/
+
+    public interface ISingTest
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+
+    public class SingTest : ISingTest
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface ISconTest
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+    public class SconTest : ISconTest
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface ITranTest
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+    public class TranTest : ITranTest
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+    
+    public interface IAService
+    {
+        void RedisTest();
+    }
+    public class AService : IAService
+    {
+        private ISingTest sing; ITranTest tran; ISconTest scon;
+        public AService(ISingTest sing, ITranTest tran, ISconTest scon)
+        {
+            this.sing = sing;
+            this.tran = tran;
+            this.scon = scon;
+        }
+
+        public void RedisTest()
+        {
+
+        }
+    }
+}
